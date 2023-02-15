9df9a731fea45aad638dde53bfb391da58c6bf10
kdcross <kenneth.d.cross@gmail.com>
2023-02-15 04:23:48 -0600

    continue, break, for loops

---
+++ JavaScript/JAVASCRIPT 101/12 - For Loops/script.js
@@ -0,0 +1,21 @@
+let population = 100;
+
+//Grow population as below:
+//population = population * 1.05;
+//population *= 1.05;
+
+//Challenge to do as a while loop
+// let i = 0;
+
+// while (i < 10) {
+//   population *= 1.05;
+//   i++;
+// }
+
+// console.log(population);
+
+for(let i = 0; i < 10; i++){
+  population *= 1.05;
+}
+
+console.log(population);
